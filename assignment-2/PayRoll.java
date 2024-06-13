3302dbe74192f57f94f2c29bb184ee4eda608a9d
Shreethaar <shreethaara380@gmail.com>
2024-06-13 18:27:03 +0000

    PayRoll user-defined class v1.0

---
+++ assignment-2/PayRoll.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;import java.util.Map;
+public class PayRoll {
+    private Map<String,Salesman> salesRepInfo;
+    public PayRoll() { salesRepInfo=new HashMap<>(); }
+}
+
+
+
