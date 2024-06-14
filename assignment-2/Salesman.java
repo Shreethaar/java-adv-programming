0150795d0fffb74e8d99b3b477f05f139f405e89
Shreethaar <shreethaara380@gmail.com>
2024-06-14 15:59:11 +0000

    Salesman.java v2.0 - updates on MonthlySalary && AnnualSalary

--- assignment-2/Salesman.java
+++ assignment-2/Salesman.java
@@ -5,9 +5,9 @@ public class Salesman {
     private String salesRepBankAcc;
     private int salesRepTotalSalesUnit;         //total amount of cars sold (units)
     private double salesRepTotalSalesAmount;    //total price of cars sold (sales)
-    private double salesRepMonthlySales;
-    private double salesRepAnnualSales;
-    public Salesman(String salesRepFullName, String salesRepStaffID, String salesRepICNum, String salesRepBankAcc, int salesRepTotalSalesUnit, double salesRepTotalSalesAmount, double salesRepMonthlySales, double salesRepAnnualSales) {this.setSalesRepFullName(salesRepFullName);this.setSalesRepStaffID(salesRepStaffID);this.setSalesRepICNum(salesRepICNum);this.setSalesRepBankAcc(salesRepBankAcc);this.setSalesRepTotalSalesUnit(salesRepTotalSalesUnit);this.setSalesRepTotalSalesAmount(salesRepTotalSalesAmount);this.setSalesRepMonthlySales(salesRepMonthlySales);this.setSalesRepAnnualSales(salesRepAnnualSales);
+    private double salesRepMonthlySalary;
+    private double salesRepAnnualSalary;
+    public Salesman(String salesRepFullName, String salesRepStaffID, String salesRepICNum, String salesRepBankAcc, int salesRepTotalSalesUnit, double salesRepTotalSalesAmount, double salesRepMonthlySalary, double salesRepAnnualSalary) {this.setSalesRepFullName(salesRepFullName);this.setSalesRepStaffID(salesRepStaffID);this.setSalesRepICNum(salesRepICNum);this.setSalesRepBankAcc(salesRepBankAcc);this.setSalesRepTotalSalesUnit(salesRepTotalSalesUnit);this.setSalesRepTotalSalesAmount(salesRepTotalSalesAmount);this.setSalesRepMonthlySalary(salesRepMonthlySalary);this.setSalesRepAnnualSalary(salesRepAnnualSalary);
     }
     public String getSalesRepFullName() {return salesRepFullName;}
     public String getSalesRepStaffID() {return salesRepStaffID;}
@@ -15,16 +15,16 @@ public class Salesman {
     public String getSalesRepBankAcc() {return salesRepBankAcc;}
     public int getSalesRepTotalSalesUnit() {return salesRepTotalSalesUnit;}
     public double getSalesRepTotalSalesAmount() {return salesRepTotalSalesAmount;}
-    public double getSalesRepMonthlySales() {return salesRepMonthlySales;}
-    public double getSalesRepAnnualSales() {return salesRepAnnualSales;}
+    public double getSalesRepMonthlySalary() {return salesRepMonthlySalary;}
+    public double getSalesRepAnnualSalary() {return salesRepAnnualSalary;}
     public void setSalesRepFullName(String salesRepFullName) {this.salesRepFullName=salesRepFullName;}
     public void setSalesRepStaffID(String salesRepStaffID) {this.salesRepStaffID=salesRepStaffID;}
     public void setSalesRepICNum(String salesRepICNum) {this.salesRepICNum=salesRepICNum;}
     public void setSalesRepBankAcc(String salesRepBankAcc) {this.salesRepBankAcc=salesRepBankAcc;}
     public void setSalesRepTotalSalesUnit(int salesRepTotalSalesUnit) {this.salesRepTotalSalesUnit=salesRepTotalSalesUnit;}
     public void setSalesRepTotalSalesAmount(double salesRepTotalSalesAmount) {this.salesRepTotalSalesAmount=salesRepTotalSalesAmount;}
-    public void setSalesRepMonthlySales(double salesRepMonthlySales) { this.salesRepMonthlySales=salesRepMonthlySales;}
-    public void setSalesRepAnnualSales(double salesRepAnnualSales) {this.salesRepAnnualSales=salesRepAnnualSales;}
+    public void setSalesRepMonthlySalary(double salesRepMonthlySalary) { this.salesRepMonthlySalary=salesRepMonthlySalary;}
+    public void setSalesRepAnnualSalary(double salesRepAnnualSalary) {this.salesRepAnnualSalary=salesRepAnnualSalary;}
 }
 
 
