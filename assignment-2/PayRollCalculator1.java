e5a086080ef1455651211fb3f426efe1b4bb8524
Shreethaar <shreethaara380@gmail.com>
2024-06-16 21:44:03 +0000

    Income tax progress

--- assignment-2/PayRollCalculator1.java
+++ assignment-2/PayRollCalculator1.java
@@ -29,4 +29,32 @@ public class PayRollCalculator1 {
         double incentive=calcIncentiveCommission.apply(numOfCarsSold);
         return baseSalary+commission+incentive;
     };
+
+    Function<Double, Double> calcIncomeTax = income -> {
+        if (income <= 416.67) {
+            return income * 0.0;
+        } else if (income <= 1,666.67) {
+            return income * 0.01;
+        } else if (income <= 2,916.67) {
+            return income * 0.03;
+        } else if (income <= 4,166.67) {
+            return income * 0.08;
+        } else if (income <= 5,833.33) {
+            return income * 0.13;
+        } else if (income <= 8,333.33) {
+            return income * 0.21;
+        } else if (income <= 20,833.33) {
+            return income * 0.24;
+        } else if (income <= 33,333.33) {
+            return income * 0.245;
+        } else if (income <= 50,000.00) {
+            return income * 0.25;
+        } else if (income <= 83,333.33) {
+            return income * 0.26;
+        } else if (income <= 166,666.67) {
+            return income * 0.28;
+        } else {
+            return income * 0.30;
+        }
+    };
 }
